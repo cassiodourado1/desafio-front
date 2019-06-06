018bce459bd40a03f528e7ec500541bfd26f674e
Cassio Dourado <cassiodourado1@gmail.com>
2019-06-06 19:42:06 -0300

    home - alterando modo de busca para o includes para procurar em qualquer parte da string

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="home">
-    <Header @searchByTerm="searchByTerm" />
+    <Header @searchByTerm="searchByTerm"/>
 
     <div class="home__container">
       <home-list v-if="!term && links" :dataList="links"></home-list>
       <home-list v-if="term && resultList.length > 0" :dataList="resultList"></home-list>
-      <p v-if="term && resultList.length == 0" class="home__no-results">Sem Resultados, refaça sua busca</p>
+      <p
+        v-if="term && resultList.length == 0"
+        class="home__no-results"
+      >Sem Resultados, refaça sua busca</p>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import Header from './components/header/Header';
-import HomeList from './components/HomeList/HomeList';
+import axios from "axios";
+import Header from "./components/header/Header";
+import HomeList from "./components/HomeList/HomeList";
 
 export default {
-  name: 'Home',
+  name: "Home",
 
   components: {
     Header,
-    HomeList,
+    HomeList
   },
 
   mounted() {
@@ -32,60 +35,61 @@ export default {
   data() {
     return {
       baseUrl: process.env.VUE_APP_BASE_URL,
-      term: '',
+      term: "",
       resultList: [],
       links: [
         {
           meta: {
-            author: 'Danil Ishutin',
-            title: 'Font Size Idea: px at Root, rem for Components, em for Text Elements',
-            url: 'css-tricks.com'
+            author: "Danil Ishutin",
+            title:
+              "Font Size Idea: px at Root, rem for Components, em for Text Elements",
+            url: "css-tricks.com"
           },
-          category: 'ux_ui',
+          category: "ux_ui",
           comments: 7,
           created_at: 1459857600,
           upvotes: 9
         },
         {
           meta: {
-            author: 'Christopher Alesund',
-            title: 'Case study: Redesigning the Folyo landing page',
-            url: 'medium.com'
+            author: "Christopher Alesund",
+            title: "Case study: Redesigning the Folyo landing page",
+            url: "medium.com"
           },
-          category: 'case_study',
+          category: "case_study",
           comments: 0,
           created_at: 1460289600,
           upvotes: 2
         },
         {
           meta: {
-            author: 'Olof Kajbjer',
-            title: 'Want people to use your product? Use it yourself',
-            url: 'medium.com'
+            author: "Olof Kajbjer",
+            title: "Want people to use your product? Use it yourself",
+            url: "medium.com"
           },
-          category: 'product_design',
+          category: "product_design",
           comments: 4,
           created_at: 1460030400,
           upvotes: 11
         },
         {
           meta: {
-            author: 'Mikael Greif',
-            title: 'What do you do with a failed project?',
-            url: 'mika.el'
+            author: "Mikael Greif",
+            title: "What do you do with a failed project?",
+            url: "mika.el"
           },
-          category: 'discussion',
+          category: "discussion",
           comments: 2,
           created_at: 1460203200,
           upvotes: 4
         },
         {
           meta: {
-            author: 'Kenny Schrub',
-            title: 'Some things cant be prototyped',
-            url: 'mika.el'
+            author: "Kenny Schrub",
+            title: "Some things cant be prototyped",
+            url: "mika.el"
           },
-          category: 'discussion',
+          category: "discussion",
           comments: 25,
           created_at: 1460203200,
           isOwner: true,
@@ -93,11 +97,11 @@ export default {
         },
         {
           meta: {
-            author: 'Pasha Biceps',
-            title: 'Dont let bad process or structure kill great interfaces',
-            url: 'medium.com'
+            author: "Pasha Biceps",
+            title: "Dont let bad process or structure kill great interfaces",
+            url: "medium.com"
           },
-          category: 'product_design',
+          category: "product_design",
           comments: 0,
           created_at: 1460376000,
           isOwner: true,
@@ -105,29 +109,31 @@ export default {
         },
         {
           meta: {
-            author: 'Jacky Mao',
-            title: 'How to prototype without any tools',
-            url: 'jackymao.wix'
+            author: "Jacky Mao",
+            title: "How to prototype without any tools",
+            url: "jackymao.wix"
           },
-          caegory: 'product_design',
+          caegory: "product_design",
           coments: 0,
           upvotes: 1
-        }  
+        }
       ]
     };
   },
-
 
   methods: {
     searchByTerm(searchTerm) {
       this.term = searchTerm;
 
-      this.resultList = this.links.filter(item => item.category === this.term || item.meta.url === this.term || item.meta.author === this.term);
-
-
+      this.resultList = this.links.filter(
+        item =>
+          item.category === this.term ||
+          item.meta.title.includes(this.term) ||
+          item.meta.url.includes(this.term) ||
+          item.meta.author.includes(this.term)
+      );
     }
-
-  },
+  }
 };
 </script>
 
